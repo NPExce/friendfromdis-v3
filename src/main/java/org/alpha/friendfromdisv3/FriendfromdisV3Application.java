ab1cb04d1d92c59d2cef7fa7021def589132ca3a
arostkowski <adrian.rostkowski01@gmail.com>
2025-11-02 18:27:34 +0100

    Initial project setup with Spring Boot framework, Gradle build system, GitHub Actions workflow for Docker image deployment, and appropriate configurations.

---
+++ src/main/java/org/alpha/friendfromdisv3/FriendfromdisV3Application.java
@@ -0,0 +1,13 @@
+package org.alpha.friendfromdisv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendfromdisV3Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FriendfromdisV3Application.class, args);
+	}
+	
+}
---
+++ src/test/java/org/alpha/friendfromdisv3/FriendfromdisV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alpha.friendfromdisv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendfromdisV3ApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
