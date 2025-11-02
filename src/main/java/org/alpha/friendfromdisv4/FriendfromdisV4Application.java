db23260bbfd1a5172c3a48cba9bdb1ffb242eb97
arostkowski <adrian.rostkowski01@gmail.com>
2025-11-02 18:31:05 +0100

    asdasdasd

---
+++ src/main/java/org/alpha/friendfromdisv4/FriendfromdisV4Application.java
@@ -0,0 +1,13 @@
+package org.alpha.friendfromdisv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendfromdisV4Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FriendfromdisV4Application.class, args);
+	}
+	
+}
---
+++ src/test/java/org/alpha/friendfromdisv4/FriendfromdisV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alpha.friendfromdisv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendfromdisV4ApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
